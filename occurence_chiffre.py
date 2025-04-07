6ea5978dd6dfca77703ecd20e3cbb69f694a0d0f
Arthur domard <Arthur.domard@gmail.com>
2025-04-07 11:55:13 +0200

    occurence chiffre

--- occurence_chiffre.py
+++ occurence_chiffre.py
@@ -1,7 +1,7 @@
 nombre=0
 chiffre=0
 def nombre_chiffres():
-   nombre=input("nombre: ") 
+   nombre=input("nombre : ") 
    chiffre=input("chiffre: ")
    occurence=str(nombre).count(str(chiffre))
    return(occurence)
