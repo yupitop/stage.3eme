aea25c978e5aa7f032376cc1764f4c0eb9414721
Arthur domard <Arthur.domard@gmail.com>
2025-04-10 09:28:06 +0200

    calculette avec chenge

--- calculette.py
+++ calculette.py
@@ -1,15 +1,27 @@
-premier_chiffre=int(input("premier nombre/chiffre: "))
-deuxieme_chiffre=int(input("deuxieme nombre/chiffre: "))
-operation=input("operation: ")
-if operation=="/" :
-    print(premier_chiffre/deuxieme_chiffre)
-elif operation=="*":
-    print(premier_chiffre*deuxieme_chiffre)
-elif operation=="+":
-    print(premier_chiffre+deuxieme_chiffre) 
-elif operation=="-":
-    print(premier_chiffre-deuxieme_chiffre)
-else:
-    print("un probleme est survenu" )
-  
+def calculette():
+    try:
+        num1 = float(input("Entrez le premier nombre : "))
+        op = input("Entrez l'opérateur (+, -, *, /) : ")
+        num2 = float(input("Entrez le deuxième nombre : "))
+
+        if op == "+":
+            result = num1 + num2
+        elif op == "-":
+            result = num1 - num2
+        elif op == "*":
+            result = num1 * num2
+        elif op == "/":
+            if num2 == 0:
+                raise ZeroDivisionError
+            result = num1 / num2
+        else:
+            raise ValueError
+
+        print("Résultat : ", result)
+    except ZeroDivisionError:
+        print("Erreur : division par zéro")
+    except ValueError:
+        print("Erreur : opérateur invalide")
+
+calculette()
   
