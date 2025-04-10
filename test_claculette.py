88ef4c037643dbf585b91b7ad37d34a5192da98a
Arthur domard <Arthur.domard@gmail.com>
2025-04-10 09:57:25 +0200

    test calculette

---
+++ test_claculette.py
@@ -0,0 +1,27 @@
+
+def calculette():
+    try:
+        num1 = float(input("Entrez le premier nombre : "))
+        op = input("Entrez l'opérateur (+, -, *, /) : ")
+        num2 = float(input("Entrez le deuxième nombre : "))
+
+        if op == "+":
+            result = num1 + num2
+        elif op == "-":
+            result = num1 - num2
+        elif op == "*":
+            result = num1 * num2
+        elif op == "/":
+            if num2 == 0:
+                raise ZeroDivisionError
+            result = num1 / num2
+        else:
+            raise ValueError
+
+        print("Résultat : ", result)
+    except ZeroDivisionError:
+        print("Erreur : division par zéro")
+    except ValueError:
+        print("Erreur : opérateur invalide")
+
+calculette()
