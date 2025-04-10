8e4acd134a38e29c4c8466d7fc42cc248a90e6b3
Arthur domard <Arthur.domard@gmail.com>
2025-04-10 09:37:13 +0200

    occurence change

--- occurence_chiffre.py
+++ occurence_chiffre.py
@@ -1,9 +1,13 @@
-nombre=0
-chiffre=0
 def nombre_chiffres():
-   nombre=input("nombre : ") 
-   chiffre=input("chiffre: ")
-   occurence=str(nombre).count(str(chiffre))
-   return(occurence)
+   try:
+       nombre=input("nombre : ")
+       if not nombre.replace('.', '', 1).replace(',', '', 1).isdigit(): 
+           print("Erreur : le nombre saisi contient des lettres")
+           return
+       chiffre=input("chiffre: ")
+       occurence=str(nombre).count(str(chiffre))
+       return(occurence)
+   except ValueError:
+       print("Erreur : le nombre saisi n'est pas un entier")
+
 print(nombre_chiffres())
-    
